b1d63c9118b674fda9095c46452e4bb568c80575
Gustavo Brunoro <git@hitnail.net>
2018-07-25 16:37:25 -0300

    Adds social media accounts to player structAdds social media accounts to player struct

---
+++ structs/social_media_account.go
@@ -0,0 +1,8 @@
+package structs
+
+// SocialMediaAccount represents a social media url
+type SocialMediaAccountStruct struct {
+	Name string `json:"name"`
+	Slug string `json:"slug"`
+	Url  string `json:"url"`
+}
